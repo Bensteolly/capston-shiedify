56ad15b55823fbbc175e8ed053d7c15837db6302
Bensteolly <benstelly23@outlook.com>
2024-04-18 09:19:43 +0100

    fixing button issues still

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () =>{
       <div className=" w-full px-4 sm:px-3 lg:px-3 py-3   ">
       <div className="flex sm:flex-row justify-between items-center w-full max-w-[1200px] mx-auto p-2 ">
         <div 
-        className="w-[12%] h-[12%]"><img src={Logo} alt="Shieldfy-logo" className="w-full h-full object-cover sm:w-[40%] sm:h-[40%]" /></div>
+        className="w-[12%] h-[12%]"><img src={Logo} alt="Shieldfy-logo" className="w-full h-full object-cover sm:w-[60%] sm:h-[60%]" /></div>
         
         <ul className="lg:flex hidden sm: gap-4 mt-4 sm:mt-0">
           <li>Features</li>
@@ -21,7 +21,7 @@ const Navbar = () =>{
           <Link to="../Pages/LoginForm.js">
             <h2 className="text-center mt-3 underline">LOGIN</h2>
           </Link>
-          <button className="primary-button">GET STARTED</button>
+          <button className="w-full max-w-[145px] h-full max-h-[48px] p-4 rounded-lg font-bold text-center text-sm text-white bg-[#09122B]">GET STARTED</button>
         </div>
       </div>
       
